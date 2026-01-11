1ee56878e889695168230e965a8f8c3fed51d091
Zuzanna <122471621+YamiHeike@users.noreply.github.com>
2026-01-11 23:24:48 +0100

    chore: added ci pipeline (#2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-restclient-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("com.h2database:h2")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
--- src/test/kotlin/com/example/hacker_news_aggregator/HackerNewsAggregatorApplicationTests.kt
+++ src/test/kotlin/com/example/hacker_news_aggregator/HackerNewsAggregatorApplicationTests.kt
@@ -2,7 +2,9 @@ package com.example.hacker_news_aggregator
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
+@ActiveProfiles("test")
 @SpringBootTest
 class HackerNewsAggregatorApplicationTests {
 
